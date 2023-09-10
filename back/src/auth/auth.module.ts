15082dfd447306545ed593f3ec6bc56d741b192b
riblanc <richard.blc25@gmail.com>
2023-09-10 19:29:30 +0200

    REWORK AUTH MODULE (BACKEND):
    Import UserModule and JwtModule on authModule in order to inject user and jwt service instances into authService.
    Provides FortyTwoStrategy to authService in order to use passport-42 in authController.

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { FortyTwoStrategy } from './strategies/fortyTwo.strategy';
+import { UserModule } from "../user/user.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
+  imports: [UserModule, JwtModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, FortyTwoStrategy],
 })
 export class AuthModule {}

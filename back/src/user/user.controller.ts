11b3cf33bad26b00e22eccd2399c79281934b284
riblanc <richard.blc25@gmail.com>
2023-09-10 05:45:18 +0200

    ADD USER MODULE (BACKEND):
    Added:
    - A minimalist user service, in order to create / read / update and delete user.
    - Some basic routes to get all users / user by specific id
    - Some experimental routes to create / delete user.

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+  Req,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import {UserDto} from "../types/user.dto";
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAllUsers() {
+    return await this.userService.getAll();
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id', ParseIntPipe) id: number) {
+    return await this.userService.getById(id);
+  }
+
+  /*
+  ** TODO: Add a 'developement' Guard for these routes, so that they can only be accessed in development mode.
+  */
+  @Post()
+  @HttpCode(200)
+  @UsePipes(new ValidationPipe())
+  async createOrUpdateUser(@Body() data: UserDto) {
+    return await this.userService.createOrUpdate(data);
+  }
+
+  @Delete(':id')
+  async deleteUserById(@Param('id', ParseIntPipe) id: number) {
+    return await this.userService.delete(id);
+  }
 }
--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import {UserDto} from "../types/user.dto";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: UserDto) {
+    return await this.prisma.user.create({ data });
+  }
+
+  async getAll() {
+    return await this.prisma.user.findMany();
+  }
+
+  async getById(id: number) {
+    console.log('type of id: ', typeof id);
+    return await this.prisma.user.findUnique({ where: { id: id } });
+  }
+
+  async update(data: UserDto) {
+    return await this.prisma.user.update({ where: { id: data.id }, data });
+  }
+
+  async delete(id: number) {
+    return await this.prisma.user.delete({ where: { id } });
+  }
+
+  async createOrUpdate(data: UserDto) {
+    const user = await this.prisma.user.findUnique({ where: { id: data.id } });
+    if (user) {
+      return await this.prisma.user.update({ where: { id: data.id }, data });
+    } else {
+      return await this.prisma.user.create({ data });
+    }
+  }
+}

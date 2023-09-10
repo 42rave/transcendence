efa5dba5a9df83d5e7f966921202f5ae4bc6f837
riblanc <richard.blc25@gmail.com>
2023-09-10 04:01:05 +0200

    ADD CONFIG (BACKEND):
    Added a 'config' folder to store configuration files related to api, 42 intra (like credentials), game settings and more..

---
+++ back/src/config/intra.config.ts
@@ -0,0 +1,4 @@
+export default {
+  uid: process.env.INTRA_UID,
+  secret: process.env.INTRA_SECRET,
+}
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import apiConfig from './config/api.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(apiConfig.port);
 }
 bootstrap();

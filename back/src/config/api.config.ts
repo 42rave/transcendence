090dd2aac7268fe8e75698b7c5950fb68b196af4
riblanc <richard.blc25@gmail.com>
2023-09-12 13:38:34 +0200

    FIX CONFIG FILES (BACKEND):
    delete type construction on environment variables, avoiding for example undefined string construction.

--- back/src/config/api.config.ts
+++ back/src/config/api.config.ts
@@ -1,3 +1,3 @@
 export default {
-  port: Number(process.env.PORT) || 3000,
+  port: process.env.PORT || 3000,
 }
--- back/src/config/db.config.ts
+++ back/src/config/db.config.ts
@@ -1,6 +1,6 @@
 export default {
-  user: String(process.env.POSTGRES_USER) || 'user',
-  password: String(process.env.POSTGRES_PASSWORD) || 'password',
-  db_name: String(process.env.POSTGRES_DB) || 'db_name',
-  port: Number(process.env.POSTGRES_PORT) || 5432,
+  user: process.env.POSTGRES_USER || 'user',
+  password: process.env.POSTGRES_PASSWORD || 'password',
+  db_name: process.env.POSTGRES_DB || 'db_name',
+  port: process.env.POSTGRES_PORT || 5432,
 }

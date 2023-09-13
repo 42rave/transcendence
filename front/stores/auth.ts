5317924a4ea25d88ffeb27eb334476cb1797aa73
riblanc <richard.blc25@gmail.com>
2023-09-13 18:51:39 +0200

    REWORK CONFIG FILE (FRONTEND):
    Added WEB_APP_URL variable in nuxt.config.ts.
    Updated .env.sample (typo on variable name).
    Updated auth store in order to remove hard coded url.

---
+++ front/stores/auth.ts
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia';
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+      user: null,
+    }),
+    getters: {
+      isAuthenticated: (state) => !!state.user,
+    },
+    actions: {
+      login(data: any) {
+        const config = useRuntimeConfig();
+        window.location.href = new URL('/auth/login', config.app.API_URL as string).toString();
+      },
+      async logout() {
+        const config = useRuntimeConfig();
+        await $fetch(new URL('/auth/logout', config.app.API_URL as string).toString(), {
+            credentials: 'include',
+        }).then((res) => {
+          this.user = null;
+        }).catch(() => {});
+      },
+      async fetchUser() {
+        const config = useRuntimeConfig();
+        await $fetch(new URL('/auth/me', config.app.API_URL).toString(), {
+            credentials: 'include',
+        }).then((res) => {
+            this.user = res as any;
+        }).catch(() => {});
+      }
+    },
+});

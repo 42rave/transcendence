e18ee0d0bd96c9a7d1a52d896fc2533e5ae10714
riblanc <richard.blc25@gmail.com>
2023-09-13 19:01:49 +0200

    ADD SIMPLE CHAT POC (FRONTEND):
    Added:
    - Component /components/chat/box.vue accessible via <ChatBox />
    - A minimalist chat poc in ChatBox rendering messages dynamically, and with a text-fields to send some messages.
    - Chat page, including ChatBox and managing chat ws.

---
+++ front/pages/chat.vue
@@ -0,0 +1,48 @@
+<template>
+  <ChatBox v-if="connected" :socket="socket" />
+  <div class="ma-auto d-flex flex-column" v-else>
+    <v-progress-circular class="ma-auto" indeterminate size="64" color="purple" />
+    Connecting
+  </div>
+</template>
+
+<script lang="ts">
+import { NuxtSocket } from 'nuxt-socket-io';
+
+export default defineNuxtComponent({
+  name: 'About',
+  data: () => ({
+    socket: null as NuxtSocket,
+    connected: false,
+  }),
+  mounted() {
+    this.connect();
+  },
+  unmounted() {
+    if (!this.socket) return;
+    this.socket.removeAllListeners();
+    this.socket.disconnect();
+  },
+  methods: {
+    connect() {
+      // instantiate socket
+      this.socket = this.$nuxtSocket({
+        name: 'chat',
+        channel: 'chat',
+      })
+
+      // Add connection listeners
+      this.socket.on('connect', () => {
+        console.log('connected');
+        this.connected = true;
+      });
+      this.socket.on('disconnect', () => {
+        console.log('disconnected');
+        this.connected = false;
+        this.connect();
+      });
+      // end of connection listeners
+    },
+  }
+})
+</script>

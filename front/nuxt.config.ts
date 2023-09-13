cf0740dae375b6dd8c9025a526d77558b26f2f6c
riblanc <richard.blc25@gmail.com>
2023-09-13 11:53:08 +0200

    ADD NUXT SOCKET MODULE (FRONTEND):
    Added the nuxt-socket-io package, and instantiated the default socket on the default layout.
    I've also added the socket prop to the page (in layout: <slot :socket="socket" />) in order to get access to the socket instance on the page components through props.

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,9 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.css'],
-  build: { transpile: ['vuetify'] },
-  modules: ['@pinia/nuxt'],
-  plugins: ['~/plugins/vuetify.ts', '~/plugins/auth.ts', '~/plugins/mitt.ts'],
+  css: [
+      'vuetify/lib/styles/main.sass',
+      '@mdi/font/css/materialdesignicons.css'
+  ],
+  build: {
+    transpile: ['vuetify']
+  },
+  modules: [
+      '@pinia/nuxt',
+      'nuxt-socket-io'
+  ],
+  plugins: [
+      '~/plugins/vuetify.ts',
+      '~/plugins/auth.ts',
+      '~/plugins/mitt.ts'
+  ],
   ssr: false,
+  io: {
+    sockets: [{
+      name: 'main',
+      url: 'http://localhost:3000',
+    }]
+  }
 })

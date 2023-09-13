4422bf381a00e36937eae6ae0e4c1fcaec01cf2e
riblanc <richard.blc25@gmail.com>
2023-09-13 18:56:18 +0200

    ADD CHAT GATEWAY (BACKEND):
    Added:
    - basic chat gateway.
    - access to server context from chatService
    - simple route '/chat/sendTest' in order to test the service ability to send packet through server context.

--- back/src/chat/chat.service.ts
+++ back/src/chat/chat.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Server } from 'socket.io';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+    server: Server;
+
+    send(data: { message: string }) {
+        this.server.emit('test:message', data);
+    }
+}

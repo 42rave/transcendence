78d74ba46cf27d201cf9955c308bf01f21f73cdd
riblanc <richard.blc25@gmail.com>
2023-09-15 01:33:05 +0200

    ADD CHAT AUTHENTICATION (BACKEND):
    Added:
    - An authService method to get token from socket, get user, and inject it on socket context.
    - Authenticated guard on chat gateway.

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -3,12 +3,13 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { FortyTwoStrategy } from './strategies/fortyTwo.strategy';
 import { UserModule } from "../user/user.module";
-import { JwtModule } from "@nestjs/jwt";
-import {JwtStrategy} from "./strategies/jwt.strategy";
+import { JwtModule, JwtService } from "@nestjs/jwt";
+import { JwtStrategy } from "./strategies/jwt.strategy";
 
 @Module({
   imports: [UserModule, JwtModule],
+  exports: [JwtService, AuthService],
   controllers: [AuthController],
-  providers: [AuthService, FortyTwoStrategy, JwtStrategy],
+  providers: [AuthService, FortyTwoStrategy, JwtStrategy, JwtService],
 })
 export class AuthModule {}

b2315a2c479c34bf9e4cc4eab7b09c6123ae8f6f
riblanc <richard.blc25@gmail.com>
2023-09-16 01:09:05 +0200

    ADD TSCONFIG PATHS (BACKEND):
    Add paths to tsconfig.json, to simply imports:
    - '@/*'         -> 'src/*'
    - '@auth/*'     -> 'src/auth/*'
    - '@chat/*'     -> 'src/chat/*'
    - '@config/*'   -> 'src/config/*'
    - '@game/*'     -> 'src/game/*'
    - '@guard/*'    -> 'src/auth/guards/*'
    - '@prisma/*'   -> 'src/prisma/*'
    - '@strategy/*' -> 'src/auth/strategies/*'
    - '@type/*'     -> 'src/types/*'
    - '@user/*'     -> 'src/user/*'

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -1,10 +1,10 @@
+import { JwtModule, JwtService } from "@nestjs/jwt";
 import { Module } from '@nestjs/common';
+import { FortyTwoStrategy } from '@strategy/fortyTwo.strategy';
+import { UserModule } from "@user/user.module";
+import { JwtStrategy } from "@strategy/jwt.strategy";
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { FortyTwoStrategy } from './strategies/fortyTwo.strategy';
-import { UserModule } from "../user/user.module";
-import { JwtModule, JwtService } from "@nestjs/jwt";
-import { JwtStrategy } from "./strategies/jwt.strategy";
 
 @Module({
   imports: [UserModule, JwtModule],
--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { UserDto } from "../types/user.dto";
+import { PrismaService } from '@prisma/prisma.service';
+import { UserDto } from "@type/user.dto";
 
 @Injectable()
 export class UserService {

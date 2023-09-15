9ea894179772e42583a1adcf120494d0d3fd8a23
riblanc <richard.blc25@gmail.com>
2023-09-15 02:34:58 +0200

    ADD CLIENT MAP (CHAT):
    Added:
    - A Map<user_id, socket_id> on chatService, to list each user's connections.
    - A method 'sendToUser' on chatService, to send an event to each connection of a specific user.

--- back/src/chat/chat.service.ts
+++ back/src/chat/chat.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { Server } from 'socket.io';
+import Socket from '../types/socket';
 
 @Injectable()
 export class ChatService {
     server: Server;
+    clientMap: Map<number, string[]> = new Map();
+
+    addSocket(socket: Socket) {
+        if (!this.clientMap.has(socket.user.id))
+            this.clientMap.set(socket.user.id, [socket.id]);
+        else
+            this.clientMap.get(socket.user.id).push(socket.id);
+    }
+
+    removeSocket(socket: Socket) {
+        if (this.clientMap.has(socket.user.id)) { this.clientMap.set(
+                socket.user.id,
+                this.clientMap.get(socket.user.id).filter((s: string) => s !== socket.id)
+            );
+        }
+    }
+
+    onConnection(socket: Socket) {
+        this.addSocket(socket);
+        console.log(`${socket.user.username}: `, this.clientMap.get(socket.user.id))
+    }
+
+    onDisconnection(socket: Socket) {
+        this.removeSocket(socket);
+        console.log(`${socket.user.username}: `, this.clientMap.get(socket.user.id))
+    }
+
+    sendToUser(userId: number, data: any): void {
+        if (this.clientMap.has(userId))
+            this.server.to(this.clientMap.get(userId)).emit('test:message', data);
+    }
 
     send(data: { message: string }) {
         this.server.emit('test:message', data);

535c7cec340d1b1158230d8f570e278b79e250a1
riblanc <richard.blc25@gmail.com>
2023-09-15 18:05:16 +0200

    ADD GUARD EXAMPLE:
    Added an example of using the AuthenticatedGuard with the sendTest route.

--- back/src/chat/chat.controller.ts
+++ back/src/chat/chat.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { AuthenticatedGuard } from "../auth/guards/authenticated.guard";
+import { Request } from "../types/request";
 
 @Controller('chat')
+@UseGuards(...AuthenticatedGuard)
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Post('sendTest')
-  async send(@Body() body: { message: string }) {
+  async send(@Req() req: Request, @Body() body: { message: string }) {
+    console.log(`${req.user.username} sent:`, body);
     this.chatService.send(body as { message: string });
   }
 }

cfb5a9b3c4559b6e72dfdb98a042b854c75bee84
Adrian WEAVER <aweaver@student.42.fr>
2023-09-15 18:14:24 +0200

    ADD GUARDS ON CONTROLLER (CHANNEL):
    Adding AuthenticatedGuard on channel controller.

--- back/src/chat/channel/channel.controller.ts
+++ back/src/chat/channel/channel.controller.ts
@@ -10,7 +10,7 @@ import { Controller,
 
 import { ChannelDto } from '../../types/channel.dto';
 import type { Request } from '../../types/request';
-import { AuthGuard } from '@nestjs/passport';
+import { AuthenticatedGuard } from '../../guards/authenticated.gard';
 import { ChannelService } from './channel.service';
 
 @Controller('chat/channel')
@@ -18,11 +18,12 @@ export class ChannelController {
 	constructor(private readonly channelService: ChannelService) {}
 
 	@Get()
+	@UseGuards(...AuthenticatedGuard)
 	testIsASuccess() {
 		return 'This is a success!'
 	}
 	@Post('join')
-	@UseGuards(AuthGuard('jwt'))
+	@UseGuards(...AuthenticatedGuard)
 	@UsePipes(new ValidationPipe())
 	async join(@Req() req: Request, @Body() data: ChannelDto) {
 		return await this.channelService.join(req.user, data);

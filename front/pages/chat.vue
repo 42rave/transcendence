c1f9d88d50aba9e1257e709c72956a2468e4c06c
riblanc <richard.blc25@gmail.com>
2023-09-17 04:42:26 +0200

    UPDATE WEBSOCKETS (FRONTEND):
    Finally, succeeded in getting the socket through props!!!!
    Then, we can instantiate the chat websocket in the default layout instead of the chat page, enabling the connection to be kept active regardless of the app page.
    It also avoids to manually handle some tricky disconnection cases.
    
    i.e. this implementation will allow us to add a notification system later on if needed.

--- front/pages/chat.vue
+++ front/pages/chat.vue
@@ -1,5 +1,5 @@
 <template>
-  <ChatBox v-if="connected" :socket="socket" />
+  <ChatBox v-if="$sockets.isChatConnected" :socket="socket" />
   <div class="ma-auto d-flex flex-column" v-else>
     <v-progress-circular class="ma-auto" indeterminate size="64" color="purple" />
     Connecting
@@ -7,43 +7,7 @@
 </template>
 
 <script lang="ts">
-import { NuxtSocket } from 'nuxt-socket-io';
-
 export default defineNuxtComponent({
-  name: 'About',
-  data: () => ({
-    socket: null as NuxtSocket,
-    connected: false,
-  }),
-  mounted() {
-    this.connect();
-  },
-  unmounted() {
-    if (!this.socket) return;
-    this.socket.removeAllListeners();
-    this.socket.disconnect();
-  },
-  methods: {
-    connect() {
-      // instantiate socket
-      this.socket = this.$nuxtSocket({
-        name: 'chat',
-        channel: 'chat',
-        withCredentials: true,
-      })
-
-      // Add connection listeners
-      this.socket.on('connect', () => {
-        console.log('connected');
-        this.connected = true;
-      });
-      this.socket.on('disconnect', (event: string) => {
-        this.connected = false;
-        if (event !== 'io server disconnect')
-          this.connect();
-      });
-      // end of connection listeners
-    },
-  }
+  props: ['socket'],
 })
 </script>

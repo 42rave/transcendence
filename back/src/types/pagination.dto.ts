958d017fc75a7b6428f8a83d8eb5010be112f490
riblanc <richard.blc25@gmail.com>
2023-09-18 15:56:23 +0200

    ADD PAGINATION DTO (BACKEND):
    Added a PaginationDto and a CursorPaginationDto.

---
+++ back/src/types/pagination.dto.ts
@@ -0,0 +1,27 @@
+import { IsNumber, IsOptional, Min, Max } from "class-validator";
+
+export class CursorPaginationDto {
+    @IsNumber()
+    @IsOptional()
+    id: number;
+
+    @IsNumber()
+    @IsOptional()
+    createdAt: number;
+}
+
+export class PaginationDto {
+    @IsNumber()
+    @IsOptional()
+    @Min(0)
+    skip: number;
+
+    @IsNumber()
+    @IsOptional()
+    @Min(0)
+    @Max(100)
+    take: number = 50;
+
+    @IsOptional()
+    cursor: CursorPaginationDto;
+}

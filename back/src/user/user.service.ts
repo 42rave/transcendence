1f630327a44a83c650cf9a12b7b5cf36c4439acc
Elena-Lou <elouisiade@live.fr>
2023-09-18 15:00:19 +0200

    FIX THE BROKEN FIX (BACK):
    User table uses the singular form to fit prisma naming conventions, db
    calls to user table had to reverted to reflect this update

--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -7,31 +7,31 @@ export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async create(data: UserDto) {
-    return await this.prisma.users.create({ data });
+    return await this.prisma.user.create({ data });
   }
 
   async getAll() {
-    return await this.prisma.users.findMany();
+    return await this.prisma.user.findMany();
   }
 
   async getById(id: number) {
-    return await this.prisma.users.findUnique({ where: { id: id } });
+    return await this.prisma.user.findUnique({ where: { id: id } });
   }
 
   async update(data: UserDto) {
-    return await this.prisma.users.update({ where: { id: data.id }, data });
+    return await this.prisma.user.update({ where: { id: data.id }, data });
   }
 
   async delete(id: number) {
-    return await this.prisma.users.delete({ where: { id } });
+    return await this.prisma.user.delete({ where: { id } });
   }
 
   async createOrUpdate(data: UserDto) {
-    const user = await this.prisma.users.findUnique({ where: { id: data.id } });
+    const user = await this.prisma.user.findUnique({ where: { id: data.id } });
     if (user) {
-      return await this.prisma.users.update({ where: { id: data.id }, data });
+      return await this.prisma.user.update({ where: { id: data.id }, data });
     } else {
-      return await this.prisma.users.create({ data });
+      return await this.prisma.user.create({ data });
     }
   }
 }

4999cea72380968c355fbd35ce7c3c14364901bd
Elena-Lou <elouisiade@live.fr>
2023-09-19 16:59:05 +0200

    WIP FETCHING CHANNELS FROM BACK TO FRONT:
    chat vue is split to display channels on the side but displayChannels is
    not called on its own.
    A button is created to call the fucntion on the click but an error is
    thrown

--- front/pages/chat.vue
+++ front/pages/chat.vue
@@ -1,13 +1,59 @@
+<script lang="ts">
+interface IChannel {
+  name: string;
+  owner: string;
+}
+export default defineNuxtComponent({
+  props: ['socket'],
+  data: () => ({
+    channelList: Array<IChannel>()
+  }),
+  methods: {
+    displayChannels() {
+      console.log("bouh");
+      
+      const channels = $fetch(new URL('/chat/channel', this.config.app.API_URL).toString(), {
+        credentials: 'include',
+        method: 'GET',
+      });
+      console.log(channels);
+      
+    }
+  }
+})
+</script>
+
+
 <template>
-  <ChatBox v-if="$sockets.isChatConnected" :socket="socket" />
-  <div class="ma-auto d-flex flex-column" v-else>
-    <v-progress-circular class="ma-auto" indeterminate size="64" color="purple" />
-    Connecting
+  <div class="parent">
+    <ChatBox v-if="$sockets.isChatConnected" :socket="socket" />
+    <div class="ma-auto d-flex flex-column" v-else>
+      <v-progress-circular class="ma-auto" indeterminate size="64" color="purple" />
+        Connecting
+    </div>
+    <div class="channel_card">
+      Available channels
+      <v-btn @click="displayChannels"> SHOW CHANNELS </v-btn>
+      <ul>
+        this is a list of the channels
+        <li v-for="channel in channels" :key="channel.id">
+        ({{channel.name}})
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-export default defineNuxtComponent({
-  props: ['socket'],
-})
-</script>
+
+<style>
+.parent {
+  display: grid;
+  grid-template-columns: 5fr 3fr;
+}
+
+.channel_card {
+  height: 1000px;
+  padding: 16px;
+  background: grey; 
+}
+</style>

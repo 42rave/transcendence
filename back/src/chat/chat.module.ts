7d118e8abe85d213bb0826a332b756f84b169e8a
aweaver <88249907+AdrianWeaver@users.noreply.github.com>
2023-09-19 11:51:30 +0200

    FIX IMPORT (CHAT):
    Fixing import of PrismaModule, now relative.
    
    Co-authored-by: Elena-Lou <75798793+Elena-Lou@users.noreply.github.com>

--- back/src/chat/chat.module.ts
+++ back/src/chat/chat.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaModule } from '@prisma/prisma.module';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 import { AuthModule } from "@auth/auth.module";

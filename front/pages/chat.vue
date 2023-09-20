d55d47ccb99f00eda44673bb6ec1151990e38087
Elena-Lou <elouisiade@live.fr>
2023-09-20 18:09:49 +0200

    ADD TABS ON THE SIDE TO SELECT CHANS OR PRVMSG:
    Chat.vue is modified so a nav-like tab menu is added, which will render
    the lists of the available channels or private conversations

--- front/pages/chat.vue
+++ front/pages/chat.vue
@@ -6,23 +6,29 @@ interface IChannel {
 export default defineNuxtComponent({
   props: ['socket'],
   data: () => ({
-    channelList: Array<IChannel>()
+    rail: true,
+    tab: "channels",
+    channelList: Array<IChannel>(),
+     config: useRuntimeConfig(),
   }),
+  mounted () {
+    this.displayChannels();
+  },
   methods: {
-    displayChannels() {
+    async displayChannels() {
       console.log("bouh");
       
-      const channels = $fetch(new URL('/chat/channel', this.config.app.API_URL).toString(), {
+      const channels = await $fetch(new URL('/chat/channel', this.config.app.API_URL).toString(), {
         credentials: 'include',
         method: 'GET',
-      });
+      }).catch();
+      this.channelList = channels;
       console.log(channels);
       
     }
   }
 })
 </script>
-
 
 <template>
   <div class="parent">
@@ -31,29 +37,47 @@ export default defineNuxtComponent({
       <v-progress-circular class="ma-auto" indeterminate size="64" color="purple" />
         Connecting
     </div>
-    <div class="channel_card">
-      Available channels
-      <v-btn @click="displayChannels"> SHOW CHANNELS </v-btn>
-      <ul>
-        this is a list of the channels
-        <li v-for="channel in channels" :key="channel.id">
-        ({{channel.name}})
-        </li>
-      </ul>
+    <v-navigation-drawer  location="right" fixed app>
+    <div class="d-flex flex-row">
+      <v-tabs v-model="tab" direction="vertical">
+        <v-tab prepend-icon="mdi-forum" value="channels"></v-tab>
+        <v-tab prepend-icon="mdi-chat" value="private_messages"></v-tab>
+      </v-tabs>
+      <v-window v-model="tab">
+        <v-window-item value="channels">
+          <v-card flat>
+            <v-card-text>
+              <p>liste des channels dispo</p>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+        <v-window-item value="private_messages">
+          <v-card flat>
+            <v-card-text>
+              <p>liste des conversations privées</p>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+      </v-window>
+
     </div>
+    </v-navigation-drawer>
+
   </div>
 </template>
 
+    <!-- <v-navigation-drawer>
+      <v-list>
+        <li v-for="channel in channelList" :key="channel.id">
+        {{channel.name}}
+        </li>
+      </v-list>
+    </v-navigation-drawer> -->
 
 <style>
-.parent {
-  display: grid;
-  grid-template-columns: 5fr 3fr;
-}
+.drawer__icon {
+  align-item: end;
+  width: 50px;
 
-.channel_card {
-  height: 1000px;
-  padding: 16px;
-  background: grey; 
 }
 </style>

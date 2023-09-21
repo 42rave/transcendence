970f26ca6d0702c002ea4748b551709bb0fc4f07
riblanc <richard.blc25@gmail.com>
2023-09-21 13:04:21 +0200

    UPDATE MESSAGE SERVICE (CHANNEL):
    Injecting the ChatService into MessageService.
    This allows the newly created message on the database to be sent to the corresponding channel room.

--- back/src/chat/channel/message/message.service.ts
+++ back/src/chat/channel/message/message.service.ts
@@ -3,10 +3,14 @@ import { PrismaService } from '@prisma/prisma.service';
 import { MessageDto } from '@type/message.dto';
 import { Message } from '@prisma/client';
 import { PaginationDto } from '@type/pagination.dto';
+import { ChatService } from '@chat/chat.service';
 
 @Injectable()
 export class MessageService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly chatService: ChatService,
+  ) {}
 
   async getMessages(channelId: number, pagination: PaginationDto) {
     return await this.prismaService.message.findMany({
@@ -21,12 +25,14 @@ export class MessageService {
     channelId: number,
     data: MessageDto,
   ): Promise<Message> {
-    return await this.prismaService.message.create({
+    const message: Message = await this.prismaService.message.create({
       data: {
         body: data.body,
         userId,
         channelId,
       },
     });
+    this.chatService.emit('chat:message', message, `${channelId}`);
+    return message;
   }
 }

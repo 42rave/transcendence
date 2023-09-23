e0b16e4f338bf5a867aa907f0ea82090ae5756e6
Elena-Lou <elouisiade@live.fr>
2023-09-23 18:35:22 +0200

    WIP FORM TO CREATE CHANNEL (FRONT):
    Form is visually ready, now it needs to handle requests

--- front/components/chat/bar.vue
+++ front/components/chat/bar.vue
@@ -10,6 +10,9 @@ export default defineNuxtComponent({
     tab: "channels",
     channelList: Array<IChannel>(),
     config: useRuntimeConfig(),
+    channelName: '',
+    channelKind: '',
+    protectedPassword: '',
   }),
   mounted () {
     this.displayChannels();
@@ -41,11 +44,12 @@ export default defineNuxtComponent({
 </script>
 
 <template>
-  <v-navigation-drawer v-model=_drawer location="right">
+  <v-navigation-drawer v-model=_drawer location="right" width="350">
     <div class="d-flex flex-row">
       <v-tabs v-model="tab" direction="vertical">
       	<v-tab prepend-icon="mdi-forum" value="channels"></v-tab>
         <v-tab prepend-icon="mdi-chat" value="private_messages"></v-tab>
+        <v-tab prepend-icon="mdi-plus" value="create_channel"></v-tab>
       </v-tabs>
       <v-divider :thickness="2" inset vertical></v-divider>
       <v-window v-model="tab">
@@ -67,6 +71,43 @@ export default defineNuxtComponent({
               <p>liste des conversations privées</p>
             </v-card-text>
           </v-card>
+        </v-window-item>
+      	<v-window-item value="create_channel">
+          <v-form @submit.prevent>
+             <v-container>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                    v-model="channelName"
+                    label="Name of your channel"
+                    required
+                    hide-details
+
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="10">
+                    <v-radio-group v-model="channelKind">
+                      <v-radio label="Public" value="public"></v-radio>
+                      <v-radio label="Protected" value="protected"></v-radio>
+                      <v-radio label="Private" value="private"></v-radio>
+                    </v-radio-group>
+                  </v-col>
+
+                  <v-col cols="12" >
+                    <v-text-field
+                      v-model="protectedPassword"
+                      label="Password (optional)"
+                      hide-details
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-btn type="submit" block>Submit</v-btn>
+                  </v-col>
+                </v-row>
+    </v-container>
+  </v-form>
         </v-window-item>
       </v-window>
     </div>

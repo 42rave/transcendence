103df09ab09c91a09b2273ca935b0601a26aebfc
riblanc <richard.blc25@gmail.com>
2023-09-24 16:02:24 +0200

    UPDATE AUTH CONTROLLER:
    Add 3 routes:
    - POST '/auth/totp' to generate a totp secret if 2fa is not yet enabled.
    - POST '/auth/totp/verify' to check if the code matches the secret previously generated and return a new token if successful.
    - DELETE '/auth/totp' to disable 2fa, a totp code must be sent in order to disable 2fa for security reasons.

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
 import { AuthenticatedGuard } from '@guard/authenticated.guard';
@@ -6,10 +6,14 @@ import { AuthService } from './auth.service';
 import type { Request } from '@type/request';
 import { User } from '@prisma/client';
 import authConfig from '@config/auth.config';
+import { UserService } from '@user/user.service';
 
 @Controller('auth')
 export class AuthController {
-	constructor(private readonly authService: AuthService) {}
+	constructor(
+		private readonly authService: AuthService,
+		private readonly userService: UserService
+	) {}
 
 	@Get('login')
 	@UseGuards(AuthGuard('42'))
@@ -31,5 +35,29 @@ export class AuthController {
 	@UseGuards(...AuthenticatedGuard)
 	me(@Req() req: Request): User {
 		return req.user;
+	}
+
+	@Post('totp')
+	@UseGuards(...AuthenticatedGuard)
+	async generateTotp(@Req() req: Request) {
+		return this.authService.generateTotp(req.user);
+	}
+
+	@Post('totp/verify')
+	@UseGuards(...AuthenticatedGuard, AuthGuard('totp'))
+	async verifyTotp(@Req() req: Request, @Res() res: Response) {
+		const token: { access_token: string } = await this.authService.validateUser(req.user, true);
+		res
+			.cookie('access_token', token.access_token, { httpOnly: true })
+			.status(200)
+			.json(await this.userService.enableTotp(req.user.id));
+	}
+
+	@Delete('totp')
+	@UseGuards(...AuthenticatedGuard, AuthGuard('totp'))
+	async disableTotp(@Req() req: Request, @Res() res: Response) {
+		const user = await this.userService.disableTotp(req.user.id);
+		const token: { access_token: string } = await this.authService.validateUser(user);
+		res.cookie('access_token', token.access_token, { httpOnly: true }).status(200).json(user);
 	}
 }

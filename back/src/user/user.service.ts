4e2a50e485434872b710d01c092ff3fb9caf3cf6
riblanc <richard.blc25@gmail.com>
2023-09-24 15:42:55 +0200

    UPDATE USER SERVICE:
    Add setSecret, getSecret, enableTotp and disableTotp methods to manage user's totp information.

--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -2,37 +2,63 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@prisma/prisma.service';
 import { UserDto } from '@type/user.dto';
 import { PaginationDto } from '@type/pagination.dto';
+import { Otp, User } from '@prisma/client';
+import { decrypt, encrypt } from '@/utils/crypt-manager';
 
 @Injectable()
 export class UserService {
 	constructor(private prisma: PrismaService) {}
 
-	async create(data: UserDto) {
-		return await this.prisma.user.create({ data });
+	async create(data: UserDto): Promise<User> {
+		return this.prisma.user.create({ data });
 	}
 
-	async getAll(pagination: PaginationDto) {
-		return await this.prisma.user.findMany(pagination as object);
+	async getAll(pagination: PaginationDto): Promise<User[]> {
+		return this.prisma.user.findMany(pagination as object);
 	}
 
-	async getById(id: number) {
-		return await this.prisma.user.findUnique({ where: { id: id } });
+	async getById(id: number): Promise<User> {
+		return this.prisma.user.findUnique({ where: { id: id } });
 	}
 
-	async update(data: UserDto) {
-		return await this.prisma.user.update({ where: { id: data.id }, data });
+	async update(data: UserDto): Promise<User> {
+		return this.prisma.user.update({ where: { id: data.id }, data });
 	}
 
-	async delete(id: number) {
-		return await this.prisma.user.delete({ where: { id } });
+	async delete(id: number): Promise<User> {
+		return this.prisma.user.delete({ where: { id } });
 	}
 
-	async createOrUpdate(data: UserDto) {
+	async createOrUpdate(data: UserDto): Promise<User> {
 		const user = await this.prisma.user.findUnique({ where: { id: data.id } });
 		if (user) {
-			return await this.prisma.user.update({ where: { id: data.id }, data });
+			return this.prisma.user.update({ where: { id: data.id }, data });
 		} else {
-			return await this.prisma.user.create({ data });
+			return this.prisma.user.create({ data });
 		}
+	}
+
+	async setSecret(userId: number, secret: string): Promise<Otp> {
+		secret = encrypt(secret);
+		return this.prisma.otp.upsert({
+			where: { userId },
+			create: { secret, userId },
+			update: { secret }
+		});
+	}
+
+	async getSecret(userId: number): Promise<Otp> {
+		const otp = await this.prisma.otp.findUnique({ where: { userId } });
+		if (!otp) return undefined;
+		otp.secret = decrypt(otp.secret);
+		return otp;
+	}
+
+	async enableTotp(userId: number): Promise<User> {
+		return this.prisma.user.update({ where: { id: userId }, data: { twoFAEnabled: true }, include: { otp: true } });
+	}
+
+	async disableTotp(userId: number): Promise<User> {
+		return this.prisma.user.update({ where: { id: userId }, data: { twoFAEnabled: false } });
 	}
 }

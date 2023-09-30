029dfeac9759b49bedf758941cf0d5c90e88df18
Elena-Lou <elouisiade@live.fr>
2023-09-30 19:48:48 +0200

    ADD CHANNEL STORE VARIABLES AND PLUGINS (FRONT):
    Plugin will alow us to acces the store from anywhere in the app without
    importing it

---
+++ front/types/channel.ts
@@ -0,0 +1,9 @@
+export declare type Channel = {
+	id:				number,
+	name:           string,
+	password:       string,
+	kind:           string,
+	createdAt:		number,        
+	channelConnection: [],
+	messages:		[],  
+}

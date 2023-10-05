eec9c3b676492b152a9dd6ef73a6e618d4eba48a
riblanc <richard.blc25@gmail.com>
2023-10-05 19:58:17 +0200

    ADD UPLOAD AVATAR ROUTE (BACKEND):
    Add an upload avatar route, in order to upload files on avatars files.

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -9,12 +9,25 @@ import {
 	ParseIntPipe,
 	Post,
 	UsePipes,
-	ValidationPipe
+	ValidationPipe,
+	UploadedFile,
+	UseInterceptors,
+	UseGuards,
+	Req
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from '@type/user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AuthenticatedGuard } from '@guard/authenticated.guard';
+import { diskStorage } from 'multer';
+import appConfig from '@config/app.config';
+import { User } from '@prisma/client';
+import type { Request } from '@type/request';
+
+declare type File = Express.Multer.File;
 
 @Controller('user')
+@UseGuards(...AuthenticatedGuard)
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
@@ -30,6 +43,46 @@ export class UserController {
 	@Get(':id')
 	async getUserById(@Param('id', ParseIntPipe) id: number) {
 		return await this.userService.getById(id);
+	}
+
+	@Post('avatar')
+	@UseInterceptors(
+		FileInterceptor('file', {
+			storage: diskStorage({
+				destination: (_req: Request, _file: File, cb) => {
+					cb(null, './avatars');
+				},
+				filename: (_req: Request, _file: File, cb) => {
+					console.log('filename: ', _file.mimetype);
+					switch (_file.mimetype) {
+						case 'image/png':
+							return cb(null, `${_req.user.id}.png`);
+						case 'image/jpeg':
+							return cb(null, `${_req.user.id}.jpeg`);
+						case 'image/gif':
+							return cb(null, `${_req.user.id}.gif`);
+					}
+					return cb(null, `${_req.user.id}`);
+				}
+			}),
+			limits: {
+				fileSize: 5 * 1024 * 1024, // 5MB
+				files: 1
+			},
+			fileFilter(_req: Request, _file: File, cb: (error: Error | null, acceptFile: boolean) => void) {
+				console.log('fileFilter: ', _file.mimetype);
+				if (_file.mimetype !== 'image/png' && _file.mimetype !== 'image/jpeg' && _file.mimetype !== 'image/gif')
+					return cb(new BadRequestException('Invalid file type. Only jpeg, png and gif are allowed.'), false);
+				cb(null, true);
+			}
+		})
+	)
+	async uploadAvatar(@Req() req: Request, @UploadedFile() file: File): Promise<{ user: User; file: File }> {
+		const user = await this.userService.update({
+			id: req.user.id,
+			avatar: `${appConfig.BASE_URL}/${file.path}`
+		});
+		return { file, user };
 	}
 
 	/*
--- back/src/user/user.module.ts
+++ back/src/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '@prisma/prisma.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { DeviceService } from '@auth/device/device.service';
 
 @Module({
 	imports: [PrismaModule],
 	controllers: [UserController],
-	providers: [UserService],
+	providers: [DeviceService, UserService],
 	exports: [UserService]
 })
 export class UserModule {}

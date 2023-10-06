050ec2deb9c63fcf11dfe1be5671c3ed19cc7be7
Adrian WEAVER <aweaver@student.42.fr>
2023-10-06 15:01:11 +0200

    ADD LADDER (GAME):
    Adding method to get the ladder.
    It's an array of LadderDisplay
    { id: number, username: string, winNb: number }

--- back/src/game/game.service.ts
+++ back/src/game/game.service.ts
@@ -2,7 +2,7 @@ import { Injectable, ForbiddenException, BadRequestException } from '@nestjs/com
 import { PrismaService } from '@prisma/prisma.service';
 import { ChatService } from '@chat/chat.service';
 import type { LadderDisplay, HistoryDisplay, GameStats } from '@type/game';
-import { } from '@prisma/client';
+import { GameState } from '@prisma/client';
 
 @Injectable()
 export class GameService {
@@ -11,44 +11,52 @@ export class GameService {
 		private readonly chatService: ChatService
 	) {}
 	
-	// This returns a LadderDisplay array.
-	// LadderDisplay {
-	//   id: number;
-	//   username: string;
-	//   winNb: number;
-	// }
-	//
-	// This needs to be sorted in descending order based on winNb
+	/* This returns a LadderDisplay array.
+	   LadderDisplay {
+	     id: number;
+	     username: string;
+	     winNb: number;
+	   }
+	  
+	   This needs to be sorted in descending order by the front based on winNb */
 	async getLadder(): Promise<LadderDisplay[]> {
-		ladder: Array<LadderDisplay>
+		let ladder: Array<LadderDisplay> = []
 
-		const allMatches = this.prisma.user.findMany({
-			where : {},
-			include: { gameRecords: true }
+		const allUsers = await this.prisma.user.findMany({
+			where : { gameRecords: { some: {} } },
+			include: {
+				_count: {
+					select: { gameRecords: { where: { result: GameState.WON } } }
+				},
+			},
 		});
-
-		return null;
+		console.log(allUsers);
+		for (var i = 0; i < allUsers.length; i++) {
+			ladder.push({id: allUsers[i].id, username: allUsers[i].username,
+					winNb: allUsers[i]._count.gameRecords });
+		}
+		return ladder;
 	}
 
-	// This returns a HistoryDisplay array
-	// HistoryDisplay {
-	//   data: Date,
-	//   player_1: { id: number, username: string, score: number },
-	//   player_2: { id: number, username: string, score: number },
-	//   state: GameState
-	// }
-	// GameState is a prisma enum { WON, LOST, DRAW }
+	/* This returns a HistoryDisplay array
+	   HistoryDisplay {
+	     data: Date,
+	     player_1: { id: number, username: string, score: number },
+	     player_2: { id: number, username: string, score: number },
+	     state: GameState
+	   }
+	   GameState is a prisma enum { WON, LOST, DRAW } */
 	async getHistory(userId: number): Promise<HistoryDisplay[]> {
 		return null;
 	}
-	// This returns a GameState 
-	// GameState {
-	//   data: Date,
-	//   player_1: { id: number, username: string, score: number },
-	//   player_2: { id: number, username: string, score: number },
-	//   state: GameState
-	// }
-	// GameState is a prisma enum { WON, LOST, DRAW }
+	/* This returns a GameState 
+	   GameState {
+	     data: Date,
+	     player_1: { id: number, username: string, score: number },
+	     player_2: { id: number, username: string, score: number },
+	     state: GameState
+	   }
+	   GameState is a prisma enum { WON, LOST, DRAW } */
 
 	async getStats(userId: number): Promise<GameStats> {
 		return null;

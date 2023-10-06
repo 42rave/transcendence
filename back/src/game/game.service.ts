b3ce645cfc4e008c0970a48410734bb444530cec
Adrian WEAVER <aweaver@student.42.fr>
2023-10-06 16:08:49 +0200

    REWORK NORM && REMOVE COMMENTS (GAME BACKEND):
    Removed several comments that should have not been commited in
    the first place :D
    
    Also fixed lint errors while at it.

--- back/src/game/game.service.ts
+++ back/src/game/game.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, ForbiddenException, BadRequestException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@prisma/prisma.service';
 import { ChatService } from '@chat/chat.service';
 import type { LadderDisplay, HistoryDisplay, GameStats } from '@type/game';
@@ -10,7 +10,7 @@ export class GameService {
 		private readonly prisma: PrismaService,
 		private readonly chatService: ChatService
 	) {}
-	
+
 	/* This returns a LadderDisplay array.
 	   LadderDisplay {
 	     id: number;
@@ -20,19 +20,18 @@ export class GameService {
 	  
 	   This needs to be sorted in descending order by the front based on winNb */
 	async getLadder(): Promise<LadderDisplay[]> {
-		let ladder: Array<LadderDisplay> = []
+		const ladder: Array<LadderDisplay> = [];
 
 		const allUsers = await this.prisma.user.findMany({
-			where : { gameRecords: { some: {} } },
+			where: { gameRecords: { some: {} } },
 			include: {
 				_count: {
 					select: { gameRecords: { where: { result: GameState.WON } } }
-				},
-			},
+				}
+			}
 		});
-		for (var i = 0; i < allUsers.length; i++) {
-			ladder.push({id: allUsers[i].id, username: allUsers[i].username,
-					winNb: allUsers[i]._count.gameRecords });
+		for (let i = 0; i < allUsers.length; i++) {
+			ladder.push({ id: allUsers[i].id, username: allUsers[i].username, winNb: allUsers[i]._count.gameRecords });
 		}
 		return ladder;
 	}
@@ -46,29 +45,32 @@ export class GameService {
 	   }
 	   GameState is a prisma enum { WON, LOST, DRAW } */
 	async getHistory(userId: number): Promise<HistoryDisplay[]> {
-		let gameHistory: Array<HistoryDisplay> = []
+		const gameHistory: Array<HistoryDisplay> = [];
 
 		const allGames = await this.prisma.game.findMany({
-			where : { 
+			where: {
 				records: {
 					some: { playerId: userId }
-					}
-				},
+				}
+			},
 			include: { records: { include: { player: true } } }
 		});
-		for (var i = 0; i < allGames.length; i++) {
-			gameHistory.push({date: allGames[i].createdAt,
+		for (let i = 0; i < allGames.length; i++) {
+			gameHistory.push({
+				date: allGames[i].createdAt,
 				player_1: {
 					id: allGames[i].records[0].playerId,
 					username: allGames[i].records[0].player.username,
-					score: allGames[i].records[0].score,
+					score: allGames[i].records[0].score
 				},
 				player_2: {
 					id: allGames[i].records[1].playerId,
 					username: allGames[i].records[1].player.username,
-					score: allGames[i].records[1].score,
+					score: allGames[i].records[1].score
 				},
-				state: (allGames[i].records[0].playerId) === userId ? allGames[i].records[0].result : allGames[i].records[1].result })
+				state:
+					allGames[i].records[0].playerId === userId ? allGames[i].records[0].result : allGames[i].records[1].result
+			});
 		}
 		return gameHistory;
 	}
@@ -83,6 +85,9 @@ export class GameService {
 	   GameState is a prisma enum { WON, LOST, DRAW } */
 
 	async getStats(userId: number): Promise<GameStats> {
-		return null;
+		let gameStats: GameStats;
+		void userId;
+
+		return gameStats;
 	}
 }

b41682573a98cbcdbf8dc7217510a852fc9577e6
Adrian WEAVER <aweaver@student.42.fr>
2023-10-08 16:00:39 +0200

    REWORK HISTORY && SORT HISTORY BY DATE:
    Reworked history based on knew map and sort knowledge.
    Now sorting matches based on dates, more recents matches show first in
    match history.

--- back/src/game/game.service.ts
+++ back/src/game/game.service.ts
@@ -46,34 +46,31 @@ export class GameService extends BroadcastService {
 	   }
 	   GameState is a prisma enum { WON, LOST, DRAW } */
 	async getHistory(userId: number): Promise<HistoryDisplay[]> {
-		const gameHistory: Array<HistoryDisplay> = [];
-
-		const allGames = await this.prisma.game.findMany({
-			where: {
-				records: {
-					some: { playerId: userId }
-				}
-			},
-			include: { records: { include: { player: true }, orderBy: { position: 'asc' } } }
-		});
-		for (let i = 0; i < allGames.length; i++) {
-			gameHistory.push({
-				date: allGames[i].createdAt,
+		return (
+			await this.prisma.game.findMany({
+				where: {
+					records: {
+						some: { playerId: userId }
+					}
+				},
+				include: { records: { include: { player: true }, orderBy: { position: 'asc' } } }
+			})
+		)
+			.map((games) => ({
+				date: games.createdAt,
 				player_1: {
-					id: allGames[i].records[0].playerId,
-					username: allGames[i].records[0].player.username,
-					score: allGames[i].records[0].score
+					id: games.records[0].playerId,
+					username: games.records[0].player.username,
+					score: games.records[0].score
 				},
 				player_2: {
-					id: allGames[i].records[1].playerId,
-					username: allGames[i].records[1].player.username,
-					score: allGames[i].records[1].score
+					id: games.records[1].playerId,
+					username: games.records[1].player.username,
+					score: games.records[1].score
 				},
-				state:
-					allGames[i].records[0].playerId === userId ? allGames[i].records[0].result : allGames[i].records[1].result
-			});
-		}
-		return gameHistory;
+				state: games.records[0].playerId === userId ? games.records[0].result : games.records[1].result
+			}))
+			.sort((a, b) => (a.date < b.date ? 1 : -1));
 	}
 
 	async getGameNbByResult(userId: number, result: GameState): Promise<number> {

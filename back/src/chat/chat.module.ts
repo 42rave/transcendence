984b8a687e9c6ac06118563177c6979f8960de41
Adrian WEAVER <aweaver@student.42.fr>
2023-10-08 16:07:39 +0200

    REWORK USER SERVICE:
    Moving a method to get blocked users as a list to the user service
    instead of the message service that seem to not make sense.
    
    Added proper modules in order to use the userService in the message service.

--- back/src/chat/chat.module.ts
+++ back/src/chat/chat.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from '@auth/auth.module';
 import { ChannelController } from '@chat/channel/channel.controller';
 import { MessageController } from '@chat/message/message.controller';
 import { PrivmsgController } from '@chat/privmsg/privmsg.controller';
+import { UserService } from '@user/user.service';
 import { ChannelService } from '@chat/channel/channel.service';
 import { MessageService } from '@chat/message/message.service';
 import { PrivmsgService } from '@chat/privmsg/privmsg.service';
@@ -15,7 +16,7 @@ import { StatusModule } from '@user/status/status.module';
 @Module({
 	imports: [AuthModule, PrismaModule, StatusModule],
 	controllers: [ChatController, ChannelController, MessageController, PrivmsgController],
-	providers: [ChatGateway, SocialService, ChannelService, MessageService, PrivmsgService],
+	providers: [ChatGateway, SocialService, ChannelService, MessageService, PrivmsgService, UserService],
 	exports: [SocialService]
 })
 export class ChatModule {}
--- back/src/chat/message/message.service.ts
+++ back/src/chat/message/message.service.ts
@@ -1,28 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@prisma/prisma.service';
 import { MessageDto } from '@type/message.dto';
-import { Message, RelationKind, User } from '@prisma/client';
+import { Message, User } from '@prisma/client';
 import { SocialService } from '@chat/social.service';
+import { UserService } from '@user/user.service';
 
 @Injectable()
 export class MessageService {
 	constructor(
 		private readonly prismaService: PrismaService,
-		private readonly socialService: SocialService
+		private readonly socialService: SocialService,
+		private readonly userService: UserService
 	) {}
 
-	async getBlockedUsersIds(user: User): Promise<number[]> {
-		return (
-			await this.prismaService.relationship.findMany({
-				where: {
-					AND: [{ senderId: user.id }, { kind: RelationKind.BLOCKED }]
-				}
-			})
-		).map((blocked) => blocked.receiverId);
-	}
-
 	async getMessages(user: User, channelId: number) {
-		const blockedUsersIds: number[] = await this.getBlockedUsersIds(user);
+		const blockedUsersIds: number[] = await this.userService.getBlockedUsersIds(user);
 		return await this.prismaService.message.findMany({
 			where: {
 				AND: [{ channelId }, { userId: { notIn: blockedUsersIds } }]

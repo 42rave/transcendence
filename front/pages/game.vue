5aff8e53c9a17fdd3b715030cb730dbfaaf18f02
riblanc <richard.blc25@gmail.com>
2023-10-09 13:40:45 +0200

    ADD BASIC GAME COMPONENT STRUCTURE (FRONTEND):
    Add a basic structure for the game component.

--- front/pages/game.vue
+++ front/pages/game.vue
@@ -5,8 +5,14 @@ export default defineNuxtComponent({
 </script>
 
 <template>
-  <div id="game-container">
-    <GamePlay :socket='this.gameSocket' :left='true' />
+  <div v-if="this.$sockets.gameConnected" class="gamePage">
+    <div id="game-container">
+      <GamePlay :socket='this.gameSocket' :left='true' />
+    </div>
+  </div>
+  <div class="ma-auto d-flex flex-column" v-else>
+    <v-progress-circular class="ma-auto" indeterminate size="64" color="purple" />
+    Connecting
   </div>
 </template>
 
@@ -17,5 +23,14 @@ export default defineNuxtComponent({
   max-width: 100%;
   max-height: calc(100vh - 64px);
   display: grid;
+}
+.gamePage {
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+  max-height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
